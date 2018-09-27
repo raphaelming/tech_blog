30e9bb25c563e1edb4756c4aa4802e74d7251bcf
Lming1 <spear131121@gmail.com>
2018-09-28 01:16:10 +0900

    tech blog Spring boot setting init

---
+++ techblog/src/main/java/com/raphael/blog/techblog/TechblogApplication.java
@@ -0,0 +1,12 @@
+package com.raphael.blog.techblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechblogApplication.class, args);
+    }
+}
