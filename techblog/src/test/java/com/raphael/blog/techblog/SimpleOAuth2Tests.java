dca9d975ca9bdb0a9a965744b5c6e62492b8b86e
Lming1 <spear131121@gmail.com>
2019-02-07 21:48:41 +0900

    Simple OAuth2 Tests modified

--- techblog/src/test/java/com/raphael/blog/techblog/SimpleOAuth2Tests.java
+++ techblog/src/test/java/com/raphael/blog/techblog/SimpleOAuth2Tests.java
@@ -19,18 +19,10 @@ import java.util.Arrays;
 public class SimpleOAuth2Tests {
 
 
+
     @Test
     public void simpleOAuth2Test() {
-        ResourceOwnerPasswordResourceDetails resource = new ResourceOwnerPasswordResourceDetails();
-        resource.setUsername("user");
-        resource.setPassword("test");
-        resource.setAccessTokenUri("http://localhost:8080/oauth/token");
-        resource.setClientId("foo");
-        resource.setClientSecret("bar");
-        resource.setGrantType("password");
-        resource.setScope(Arrays.asList(new String[]{"read", "write", "trust"}));
-
-
+        ResourceOwnerPasswordResourceDetails resource = getResourceOwnerPasswordResourceDetails();
         DefaultOAuth2ClientContext clientContext = new DefaultOAuth2ClientContext();
         OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resource, clientContext);
         Sample sample = restTemplate.getForObject("http://localhost:8080/", Sample.class);
@@ -39,6 +31,15 @@ public class SimpleOAuth2Tests {
 
     @Test
     public void memberOAuth2Test() {
+        ResourceOwnerPasswordResourceDetails resource = getResourceOwnerPasswordResourceDetails();
+        DefaultOAuth2ClientContext clientContext = new DefaultOAuth2ClientContext();
+        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resource, clientContext);
+        Member member = restTemplate.getForObject("http://localhost:8080/members/2", Member.class);
+        Assert.assertEquals("test", member.getUserId());
+
+    }
+
+    private ResourceOwnerPasswordResourceDetails getResourceOwnerPasswordResourceDetails() {
         ResourceOwnerPasswordResourceDetails resource = new ResourceOwnerPasswordResourceDetails();
         resource.setUsername("user");
         resource.setPassword("test");
@@ -47,11 +48,6 @@ public class SimpleOAuth2Tests {
         resource.setClientSecret("bar");
         resource.setGrantType("password");
         resource.setScope(Arrays.asList(new String[]{"read", "write", "trust"}));
-
-        DefaultOAuth2ClientContext clientContext = new DefaultOAuth2ClientContext();
-        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resource, clientContext);
-        Member member = restTemplate.getForObject("http://localhost:8080/members/2", Member.class);
-        Assert.assertEquals("test", member.getUserId());
-
+        return resource;
     }
 }
